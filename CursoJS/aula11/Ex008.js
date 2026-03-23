1e2e56fdd005130a1044f68001ea062df91f7e4a
LUIZ ALBERTH NEPPEL <10816533962@edu.udesc.br>
2026-03-23 14:39:10 -0300

    adição de classes

---
+++ CursoJS/aula11/Ex008.js
@@ -0,0 +1,6 @@
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel >60){
+    console.log('Você ultrapassou o limite de velocidade. Multado!')
+}
+console.log('Se beber não dirija')
