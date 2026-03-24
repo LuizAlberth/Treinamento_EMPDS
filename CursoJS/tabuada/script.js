9f0e12db0719d5db9dbe05e3cb620a23f1154bef
LUIZ ALBERTH NEPPEL <10816533962@edu.udesc.br>
2026-03-24 18:50:00 -0300

    alterações

---
+++ CursoJS/tabuada/script.js
@@ -0,0 +1,9 @@
+function tabuada(){
+    var txtn = document.getElementById('valor')
+    var valor = Number(txtn.value)
+    var res = document.getElementById('res')
+
+    res = `A tabuada de ${valor} é: <br>`
+
+    for()
+}
